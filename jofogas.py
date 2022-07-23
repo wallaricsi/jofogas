2ae253273f34f403ccefbbd31aca2fe4e77ed64d
wallaricsi <65926515+wallaricsi@users.noreply.github.com>
2022-07-23 17:52:35 +0200

    open browser

--- jofogas.py
+++ jofogas.py
@@ -4,3 +4,23 @@ from selenium.webdriver.chrome.options import Options
 import openpyxl
 import requests
 from bs4 import BeautifulSoup
+
+
+opt = Options()
+opt.binary_location = 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
+arr = ['enable-automation', 'disable-popup-blocking', 'disable-notifications']
+opt.add_experimental_option("excludeSwitches", arr);
+driver = webdriver.Chrome(
+    options=opt, executable_path='c:/webDriver/chromedriver.exe')
+driver.get('https://www.jofogas.hu/')
+
+time.sleep(2)
+
+driver.find_element_by_xpath('//*[@id="didomi-notice-agree-button"]/span').sendKeys("TAB")
+
+#sendKeys("TAB")
+#sendKeys("TAB")
+
+#switch = driver.switch_to.alert.sendKeys("TAB") 
+#driver.switchTo().alert().sendKeys("TAB");
+driver.find_element_by_xpath('//*[@id="didomi-notice-agree-button"]/span').click()
